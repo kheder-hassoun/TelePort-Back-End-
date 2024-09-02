3150dac7cf6ddd9dce999416b8b4d914ea496924
kheder Hassoun <103248209+kheder-hassoun@users.noreply.github.com>
2024-09-02 10:32:28 +0300

    Delete src/main directory

--- src/main/java/com/m1guelsb/springauth/dtos/SignInDto.java
+++
@@ -1,7 +0,0 @@
-package com.m1guelsb.springauth.dtos;
-
-
-public record SignInDto(
-    String userName,
-    String password) {
-}
--- src/main/java/com/m1guelsb/springauth/dtos/SignUpDto.java
+++
@@ -1,9 +0,0 @@
-package com.m1guelsb.springauth.dtos;
-
-import com.m1guelsb.springauth.enums.UserRole;
-
-public record SignUpDto(
-    String userName,
-    String password
-    ) {
-}
